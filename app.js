86d1057ce7ed539d6ff80a1babcd38c4bd20227c
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-10-27 16:56:17 -0300

    Iniciando a criação das funções

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+
+
+async function listarContatos() {
+    const url = 'http://localhost:8080/v1/whatsapp/listarContatos'
+    const response = await fetch(url)
+    const contatos = await response.json()
+    console.log(contatos.users)
+}
+
+listarContatos()
